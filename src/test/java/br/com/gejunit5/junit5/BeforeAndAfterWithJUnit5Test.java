f75ba5aea4b5e7bc3330b7f52e2c12559da6513f
Alexandre Gama <alexandre.gama.lima@gmail.com>
2017-02-09 19:54:32 -0200

    Difference between Before and After in JUnit 4 and JUnit 5

---
+++ src/test/java/br/com/gejunit5/junit5/BeforeAndAfterWithJUnit5Test.java
@@ -0,0 +1,42 @@
+package br.com.gejunit5.junit5;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BeforeAndAfterWithJUnit5Test {
+
+	@BeforeAll
+	static void executeOnceBeforeTheClassTest() {
+		System.out.println("BeforeAll - Executing once before the Class test\n");
+	}
+	
+	@BeforeEach
+	void executeBeforeEachTest() {
+		System.out.println("BeforeEach - Executing before each test");
+	}
+	
+	@Test
+	void firstTest() {
+		System.out.println("First test being executed\n");
+	}
+	
+	@Test
+	void secondTest() {
+		System.out.println("Second test being executed\n");
+	}
+	
+	@AfterEach
+	void executeAfterEachTest() {
+		System.out.println("AfterEach - Executing after all tests run\n");
+	}
+	
+	@AfterAll
+	static void executeOnceAfterTheClassTest() {
+		System.out.println("AfterAll - Executing after all tests run");
+	}
+	
+	
+}
