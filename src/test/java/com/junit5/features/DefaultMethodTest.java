a5c40280c0b85ae908299a8a24681368ee3d3782
Alexandre Gama <alexandre.gama.lima@gmail.com>
2017-03-20 23:27:00 -0300

    DisplayName

--- src/test/java/com/junit5/features/DefaultMethodTest.java
+++ src/test/java/com/junit5/features/DefaultMethodTest.java
@@ -2,15 +2,26 @@ package com.junit5.features;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName(value = "In Classes - This class will test in a noble style")
 class DefaultMethodTest {
 
+	@DisplayName("In Methods - This custom name does not use the value attribute")
 	@Test
-	void shouldBeTheCorrectTutorial() {
+	void shouldBeTheCorrectJUnit5Tutorial() {
 		String tutorial = "JUnit 5";
 		
 		assertEquals("JUnit 5", tutorial);
+	}
+	
+	@DisplayName("JUnit 5 is really Smart - It accepts emojis 😏")
+	@Test
+	void shouldBeTheCorrectMockitoTutorial() {
+		String tutorial = "Mockito";
+		
+		assertEquals("Mockito", tutorial);
 	}
 	
 }
